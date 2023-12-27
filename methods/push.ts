bc53afd474d207619fd49af0e63eb2c01ee6f01e
Marconi Maciel Guimarães Junior <102702355+MarconiJunior@users.noreply.github.com>
2023-12-27 08:39:27 -0300

    Update push.ts

--- methods/push.ts
+++ methods/push.ts
@@ -1,5 +1,8 @@
-export function push<T>(array: T[], ...newElements: T[]): T[] {
-  return [...array, ...newElements];
+export function push<T>(array: T[], ...newElements: T[]): number {
+  for (const element of elements) {
+    array[array.length] = element
+  }
+  return arr.length
 }
 
 let pushArray = [1, 4, 5];
